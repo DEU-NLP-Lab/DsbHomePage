d4ee8cd9bae50a8bbcaaafdeb44e9b5f400f2b0e
Minsu <jminsu084@gmail.com>
2024-02-01 21:29:29 +0900

    게시판 페이지 만들기

--- homePage/post/views.py
+++ homePage/post/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from .models import Post
+
 
 # Create your views here.
+def index(request):
+    posts = Post.objects.all().order_by('-pk')
+    print(posts)
+
+    return render(
+        request,
+        'post/index.html',
+        {
+            'posts': posts
+        },
+    )
